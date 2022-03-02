f5b5df449067291360cb79a2ee3af8570c0f3bcf
Mahmudur <mahmudhera93@gmail.com>
2022-03-02 13:24:45 -0500

    added simple test for 3rd moment exact

--- expected_probability_corner_cases/pdf_nmut/pdf_nmut.py
+++ expected_probability_corner_cases/pdf_nmut/pdf_nmut.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import hyp2f1
 
 def r1_to_q(k,r1):
 	#return 1-(1-r1)**k
@@ -23,6 +24,13 @@ def var_n_mutated(L,k,r1,q=None):
 
 def exp_n_mutated_squared(L, k, p):
     return var_n_mutated(L, k, p) + exp_n_mutated(L, k, p) ** 2
+
+def third_moment_nmut_exact(L,k,p):
+    t1 = (L * (-2 + 3*L) * p**2 + 3 * (1 - p)**(2*k) * (2 + (-1 + k - L) * p * (2 + k * p - L * p)) - (1 - p)**k * (6 + p * (-6 + L * (-6 + p + 6 * L * p))))/(p**2)
+    t2 = (-2 + 2 * k - L) * (-1 + 2 * k - L) * (2 * k - L) * (-1 + (1 - p)**k)**3
+    t3 = (1/(p**3))*(-6 * (-1 + k)**2 * (k - L) * p**3 + 6 * (1 - p)**(3 * k) * (2 + (-2 + 2 * k - L) * p) + (1 - p)**(2 * k) * (-12 + 6 * (2 * k + L) * p + 6 * (4 * k**2 + 2 * (1 + L) - 3 * k * (2 + L)) * p**2 - (-1 + k) * k * (-2 + 4 * k - 3 * L) * p**3) + 6 * (-1 + k) * (1 - p)**k * p * (-2 + p * (2 - k + 2 * L + (k * (-2 + 3 * k - 3 * L) + L) * p)))
+    t4 = 6 * (-1 + (1 - p)**k) * ((k + k**2 - 2 * k * L + (-1 + L) * L) * (-1 + 2 * (1 - p)**k) * hyp2f1(1, 2 + k - L, k - L, 1) + (k + k**2 - 2 * k * L + (-1 + L) * L) * (1 - p)**k * (-1 + p) * hyp2f1(1, 2 + k - L, k - L, 1 - p) - (-2 * k + 4 * k**2 + L - 4 * k * L + L**2) * ((-1 + 2 * (1 - p)**k) * hyp2f1(1, 1 + 2 * k - L, -1 + 2 * k - L, 1)- (-1 + p)**(2 * k) * hyp2f1(1, 1 + 2 * k - L, -1 + 2 * k - L, 1 - p)))
+    return t1+t2+t3+t4
 
 def perform_exhaustive_counting(k, p, L):
     arr = np.zeros((2, L+1))
@@ -74,6 +82,13 @@ print( exp_n_mutated_squared(L,k,p) )
 print('2nd moment from pdf:')
 pdf = get_nmut_pdf(L+k-1, k, p)
 expectation = sum( [i**2*pdf[i] for i in range(L+1)] )
+print(expectation)
+
+print('3rd moment from formula:')
+print( third_moment_nmut_exact(L,k,p) )
+print('3rd moment from pdf:')
+pdf = get_nmut_pdf(L+k-1, k, p)
+expectation = sum( [i**3*pdf[i] for i in range(L+1)] )
 print(expectation)
 
 '''
